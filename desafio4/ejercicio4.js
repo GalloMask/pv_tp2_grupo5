0c5da93edc3f4724883d8e47d1bfab6b06897947
fátima <fatimacatacata51@gmail.com>
2025-09-01 15:30:57 -0300

    Al final era porque estabamos poniendo las variables afuera de la funcion y eso provocaba que no se actu. Catacata, Chimale, Bajerano

--- desafio4/ejercicio4.js
+++ desafio4/ejercicio4.js
@@ -3,16 +3,19 @@
 // valor con la propiedad JS value y mostrar los datos con un alert de la siguiente forma: “Los datos
 // ingresados son: Nombre: María Apellido: Diaz Libreta Universitaria: APU999999
 
-let nombre = document.getElementById('nombreUsuario').value;
-let apellido = document.getElementById("apellidoUsuario").value;
-let num = document.getElementById("numLU").value;
-let resultado = document.getElementById("resultado");
+
+
 
 function obtenerDatos() {
+    let nombre = document.getElementById('nombreUsuario').value;
+    let apellido = document.getElementById("apellidoUsuario").value;
+    let num = document.getElementById("numLU").value;
+    let resultado = document.getElementById("resultado");
+
     if( !nombre || !apellido || !num ){
         resultado.style.color = "red";
         resultado.innerHTML = "Completa todos los campos.";
         return;
     }
-        alert(`Los datos ingresados son: Nombre: ${nombre}, Apellido: ${apellido}, Libreta Universitaria: ${num}`);
+        alert(`Los datos ingresados son:\n Nombre: ${nombre}\n Apellido: ${apellido}\n Libreta Universitaria: ${num}`);
 }
