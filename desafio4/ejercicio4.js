98862761a3db8671d4dda361d66cca6cd0165da3
fátima <fatimacatacata51@gmail.com>
2025-09-01 14:57:58 -0300

    Haciendo el punto cuatro. Armamos primero html ahora estamos haciendo el js. Me olvide de poner nuestros nombre antes; Catacata, Aban y Caro

---
+++ desafio4/ejercicio4.js
@@ -0,0 +1,4 @@
+// 4.- Crear una página que muestre 3 campos (input): Nombre, apellido, libreta universitaria. Crear un
+// script para obtener cada uno de los elementos input creados, manipular los elementos para obtener su
+// valor con la propiedad JS value y mostrar los datos con un alert de la siguiente forma: “Los datos
+// ingresados son: Nombre: María Apellido: Diaz Libreta Universitaria: APU999999
