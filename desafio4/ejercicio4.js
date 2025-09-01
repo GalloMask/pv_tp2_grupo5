bee1e24c1dba8b1bd08b2ba9edd1d6823d0159a9
fátima <fatimacatacata51@gmail.com>
2025-09-01 15:19:56 -0300

    Cambiamos algunos detalles, como que el LU ahora tambien sea texto normal

--- desafio4/ejercicio4.js
+++ desafio4/ejercicio4.js
@@ -3,16 +3,16 @@
 // valor con la propiedad JS value y mostrar los datos con un alert de la siguiente forma: “Los datos
 // ingresados son: Nombre: María Apellido: Diaz Libreta Universitaria: APU999999
 
-let nombre = document.getElementById("nombreUsuario").value;
+let nombre = document.getElementById('nombreUsuario').value;
 let apellido = document.getElementById("apellidoUsuario").value;
-let num = parseFloat(document.getElementById("numLU").value);
+let num = document.getElementById("numLU").value;
+let resultado = document.getElementById("resultado");
 
 function obtenerDatos() {
-    if( !nombre || !apellido || isNaN(num)){
+    if( !nombre || !apellido || !num ){
         resultado.style.color = "red";
         resultado.innerHTML = "Completa todos los campos.";
         return;
-    }else{
-        alert(`Los datos ingresados son: Nombre: ${nombre}, Apellido: ${apellido}, Libreta Universitaria: ${num}`);
     }
+        alert(`Los datos ingresados son: Nombre: ${nombre}, Apellido: ${apellido}, Libreta Universitaria: ${num}`);
 }
