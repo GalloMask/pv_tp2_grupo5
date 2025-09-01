d47d67feb0cff8d8f394e17946365b80aabff7e6
Victor Caro <sbigtheo@gmail.com>
2025-09-01 11:29:00 -0300

    Caro Victor: implementcion de la validacion de los numeros enteros

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -1,10 +1,26 @@
+function leerEntero(mensaje) {
+  while (true) {
+    const entrada = prompt(mensaje);
+    if (entrada === null) throw new Error("Operación cancelada");
+    const n = Number(entrada);
+
+    if (Number.isInteger(n)) return n;
+
+    alert("Debe ser un numero entero");
+  }
+}
+
 function calcularPromedio() {
+    try {
   const a = Number(prompt("Numero entero a:"));
   const b = Number(prompt("Numero entero b:"));
   const c = Number(prompt("Nmero entero c:"));
 
   const promedio = (a + b + c) / 3;
   alert(`El promedio de ${a}, ${b} y ${c} es: ${promedio}`);
+} catch (e) {
+    alert(e.message);
+  }
 }
 
 document.getElementById("btn").addEventListener("click", calcularPromedio);
