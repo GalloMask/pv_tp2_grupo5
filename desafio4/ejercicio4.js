624062cf50c7ddf1a8743ff809d40164d90f54bc
fátima <fatimacatacata51@gmail.com>
2025-09-01 15:06:26 -0300

    Viendo si funciona sino primera revision y depuracion

--- desafio4/ejercicio4.js
+++ desafio4/ejercicio4.js
@@ -2,3 +2,17 @@
 // script para obtener cada uno de los elementos input creados, manipular los elementos para obtener su
 // valor con la propiedad JS value y mostrar los datos con un alert de la siguiente forma: “Los datos
 // ingresados son: Nombre: María Apellido: Diaz Libreta Universitaria: APU999999
+
+let nombre = document.getElementById("nombreUsuario").value;
+let apellido = document.getElementById("apellidoUsuario").value;
+let num = parseFloat(document.getElementById("numLU").value);
+
+function obtenerDatos() {
+    if( !nombre || !apellido || isNaN(num)){
+        resultado.style.color = "red";
+        resultado.innerHTML = "Completa todos los campos.";
+        return;
+    }else{
+        alert(`Los datos ingresados son: Nombre: ${nombre}, Apellido: ${apellido}, Libreta Universitaria: ${num}`);
+    }
+}
