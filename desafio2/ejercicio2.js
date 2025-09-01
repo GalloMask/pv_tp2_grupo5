0517a0f90cd2bcd421cf9a73c51c543cf0340ddd
fátima <fatimacatacata51@gmail.com>
2025-09-01 14:25:24 -0300

    Lo termine, aunque es igual a mi compañero yo no le puse el detalle del mansaje cuando cancelas la operacion o no pones un numero entero.Pero igual me guie del suyo

--- desafio2/ejercicio2.js
+++ desafio2/ejercicio2.js
@@ -1,11 +1,10 @@
 // 2.- Solicitar al usuario que ingrese tres números enteros, luego mostrar como resultado el promedio de
 // los tres. El promedio de tres números se calcula sumando los tres números y dividiendo entre 3.
-    
-function calcularPromedio() {
     const num1 = parseFloat(prompt("Ingresa el primer numero..")); 
     const num2 = parseFloat(prompt("Ingresa el segundo numero.."));
     const num3 = parseFloat(prompt("Ingresa el tercer numero.."));
 
+function calcularPromedio() {
     const calcular = (num1+num2+num3) / 3;
     alert(`El promedio de ${num1}, ${num2} y ${num3} es: ${calcular.toFixed(2)}`);
 }
